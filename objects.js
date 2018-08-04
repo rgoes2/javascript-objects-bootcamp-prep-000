050a8bf8ffbe689d62ec38e36f132c73e8440991
Renan Góes <renangoes12@gmail.com>
2018-08-04 23:06:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var playlist = {
+<<<<<<< HEAD
   john: 'gravity',
   maroon: 'wait'
 }
@@ -13,3 +14,18 @@ function removeFromPlaylist(playlistObject,artistName){
   delete playlistObject[artistName]
   return playlistObject
 }
+=======
+  john: 'gravity'
+}
+
+ function updatePlaylist (playlistObject, artistName, songTitle) {
+ Object.assign({playlist}, playlistObject, {[artistName]: songTitle });
+
+}
+/* function removeFromPlaylist(playlist,artistName){
+  return delete playlist.artistName;
+}
+*/
+
+console.log(updatePlaylist('playlist', 'Maroon', 'Wait'));
+>>>>>>> cef634b0c5434e2c62e220ce2c28fff8479ecdd7
